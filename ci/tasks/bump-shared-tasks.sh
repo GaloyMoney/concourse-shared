b2f48569bdcef2be7646d9e86cc98e099b759285
Sandipan Dey <hey@sandipan.dev>
2022-02-04 00:05:16 +0530

    ci: added commiting after vendir sync

--- ci/tasks/bump-shared-tasks.sh
+++ ci/tasks/bump-shared-tasks.sh
@@ -5,3 +5,17 @@ set -eu
 pushd source-repo/ci
 
 vendir sync
+
+if [[ -z $(git config --global user.email) ]]; then
+  git config --global user.email "bot@galoy.io"
+fi
+if [[ -z $(git config --global user.name) ]]; then
+  git config --global user.name "CI Bot"
+fi
+
+(
+  cd $(git rev-parse --show-toplevel)
+  git add -A
+  git status
+  git commit -m "chore(tasks): Bump shared tasks"
+)
