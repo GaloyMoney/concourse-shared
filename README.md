ab56fe418a7b1730a3d7e7117eb8ca7a272cfba9
Sandipan Dey <hey@sandipan.dev>
2022-12-06 14:02:33 +0530

    feat!: choose features for repositories and sync only those files through (#17)
    * docs: required specification
    
    * chore: renames for feature setting
    
    * chore: specifying features
    
    * chore: wiring up vendir
    
    * chore: updates to file names for actions and shared task names
    
    * chore: updates to file names for actions and shared task names
    
    * chore: rename synced files to main names
    
    * fix: path for pushd
    
    * chore: use rename from ubuntu distribution

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Contains shared CI tasks synced into multiple repositories.
+### Contains shared CI tasks synced across repositories.
+
+#### Steps for adding in a new repository:
+
+1. Clone and get into the repository: 
+```
+gh repo clone GaloyMoney/concourse-shared
+cd concourse-shared
+```
+2. Edit `ci/values.yml` and under `src_repos`, add the new repository. The key name for the repository must be the name of the repository as on GitHub under GaloyMoney organization.
+This file contains many feature flags according to which the Pull Request will be created with shared tasks.
+3. Push the change on the CI.
+```
+ci/repipe
+```
+4. Make sure that `galoybot` has permissions to the target repository and it also has `galoybot` as a possible label in the PR.
+
+This would, in turn create a new job under the concourse-shared pipeline and when it runs, it would automatically create the pull request for you on the specified target repository.
+
+#### Shared Folder Details
+
+1. `actions` folder - Get synced to `.github/workflows/` folder
+2. `ci` folder - Get synced to `ci/` folder
+
+#### Feature Flags
+
+Feature Flags `nodejs`, `rust`, `chart` and `docker` are supported right now.
+Files whose names don't start with them are treated as common and synced to all.
+These are keywords such that files whose names start with these are included in that specific repository.
--- ci/tasks/bump-shared-files.sh
+++ ci/tasks/bump-shared-files.sh
@@ -11,34 +11,41 @@ pushd source-repo
 mkdir -p ci
 sed "s/ref:.*/ref: ${ref}/g" ../repo/vendir.tmpl.yml > ./ci/vendir.yml
 
+echo $FEATURES | jq -c '.[]' | while read feat_str; do
+  feat=$(echo $feat_str | tr -d '"')
+
+  # removes the features we need from excludePaths in vendir yaml
+  sed -i "/\b\($feat-*\)\b/d" ./ci/vendir.yml
+done
+
 pushd ci
 vendir sync
+rm vendir.*
+
+pushd vendor/tasks
+rename 's/^nodejs-//' *
+rename 's/^rust-//' *
+rename 's/^docker-//' *
+rename 's/^chart-//' *
 popd
 
-mkdir -p ./.github/workflows
-if [[ -f ./yarn.lock ]]; then
-  cp ./ci/vendor/actions/nodejs-*.yml ./.github/workflows/
-  # exclude audit from main backend
-  if [[ -f ./.github/workflows/audit.yml ]]; then
-    rm ./.github/workflows/nodejs-audit.yml
-  fi
-  cp ./ci/vendor/nodejs-dependabot.yml ./.github/dependabot.yml
-fi
+popd
 
-if [[ -f ./Cargo.toml ]]; then
-  cp ./ci/vendor/rust-dependabot.yml ./.github/dependabot.yml
-fi
+pushd .github/workflows
+cp -r vendor/* .
+rm -rf vendor
 
-cp ./ci/vendor/actions/spelling.yml ./.github/workflows
+rename 's/^nodejs-//' *
+rename 's/^rust-//' *
+rename 's/^docker-//' *
+rename 's/^chart-//' *
+
+popd
+
+mv ci/vendor/config/*-dependabot.yml .github/dependabot.yml || true
+
 if [[ ! -f ./typos.toml ]]; then
   touch typos.toml
-fi
-
-if [[ -f ./docker-compose.yml ]]; then
-  # exclude galoy repo
-  if [[ ! -f .github/workflows/integration-test.yml ]]; then
-    cp ./ci/vendor/actions/test-integration.yml ./.github/workflows
-  fi
 fi
 
 if [[ -z $(git config --global user.email) ]]; then
