42820a4bbcb466b7bbecfb3cd0ed033ed42973f8
Sandipan Dey <hey@sandipan.dev>
2023-04-12 13:23:00 +0530

    chore: auto synced warning on all shared files

--- shared/ci/tasks/rust-helpers.sh
+++ shared/ci/tasks/rust-helpers.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+#! Auto synced from Shared CI Resources repository
+#! Don't change this file, instead change it in github.com/GaloyMoney/concourse-shared
+
 if [[ -z $(git config --global user.email) ]]; then
   git config --global user.email "bot@galoy.io"
 fi
