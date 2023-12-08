a5c23563a206e818cac2169522bf199c78f1bd65
Kartik Shah <krtk.6160@gmail.com>
2023-12-08 15:46:50 +0530

    feat: add gh-token flake (#40)
    * feat: add gh-token flake
    
    * chore: unset gh-token as default pkg
    
    * build: add `.envrc`
    
    * chore: add .gitignore
    
    * chore: change description

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Utils";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in
+      with pkgs; {
+        packages = rec {
+          gh-token = pkgs.buildGoModule rec {
+            pname = "gh-token";
+            version = "v2.0.1";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "Link-";
+              repo = "gh-token";
+              rev = version;
+              sha256 = "sha256-GoPdnZowkXowaJ1yBjxPqUz+S7FDeqovChwNZzOHosM=";
+            };
+
+            vendorHash = "sha256-QiaGdHpDeuiX6QDLX2G4rx73QasWwQ3q8BYbv/Tws8c=";
+          };
+        };
+
+        devShells.default = mkShell {};
+      }
+    );
+}
