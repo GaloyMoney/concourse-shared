ffb41cb920ecae2c8c70b186cfbb80f85b48f266
bodymindarts <justin@galoy.io>
2024-05-02 11:09:01 +0200

    chore: add cala src_repos

--- flake.nix
+++ flake.nix
@@ -6,28 +6,39 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in
-      with pkgs; {
-        packages = rec {
-          gh-token = pkgs.buildGoModule rec {
-            pname = "gh-token";
-            version = "v2.0.1";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in
+        with pkgs; {
+          packages = rec {
+            gh-token = pkgs.buildGoModule rec {
+              pname = "gh-token";
+              version = "v2.0.1";
 
-            src = pkgs.fetchFromGitHub {
-              owner = "Link-";
-              repo = "gh-token";
-              rev = version;
-              sha256 = "sha256-GoPdnZowkXowaJ1yBjxPqUz+S7FDeqovChwNZzOHosM=";
+              src = pkgs.fetchFromGitHub {
+                owner = "Link-";
+                repo = "gh-token";
+                rev = version;
+                sha256 = "sha256-GoPdnZowkXowaJ1yBjxPqUz+S7FDeqovChwNZzOHosM=";
+              };
+
+              vendorHash = "sha256-QiaGdHpDeuiX6QDLX2G4rx73QasWwQ3q8BYbv/Tws8c=";
             };
-
-            vendorHash = "sha256-QiaGdHpDeuiX6QDLX2G4rx73QasWwQ3q8BYbv/Tws8c=";
           };
-        };
 
-        devShells.default = mkShell {};
-      }
+          devShells.default = mkShell {
+            nativeBuildInputs = [
+              pkgs.ytt
+              pkgs.alejandra
+            ];
+          };
+          formatter = alejandra;
+        }
     );
 }
