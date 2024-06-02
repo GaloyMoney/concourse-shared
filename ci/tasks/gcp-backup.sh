34b921fa89b458fd65edb731c41ac91beab67366
Rishi Kumar <rsi.dev17@gmail.com>
2024-06-03 01:13:14 +0530

    make provision for credential manager

--- ci/tasks/gcp-backup.sh
+++ ci/tasks/gcp-backup.sh
@@ -2,15 +2,20 @@
 
 set -eu
 
+CI_ROOT=$(pwd)
+
+cat <<EOF > ${CI_ROOT}/gcloud-creds.json
+${GOOGLE_CREDENTIALS}
+EOF
+
+gcloud auth activate-service-account --key-file ${CI_ROOT}/gcloud-creds.json
+
 # compress the repo in a tarball
 timestamp=$(date +%Y%m%d%H%M%S)
+backup_name="${BACKUP_REPO_NAME}-${timestamp}.tar.gz"
+tar -czf "${backup_name}" ${BACKUP_REPO_NAME}
 
-tar -czf "blink-git-repo-${timestamp}.tar.gz" blink-git-repo 
-
-# initilised the gcloud sdk
-
-# gcloud_init
-# gcloud storage cp "blink-git-repo-${timestamp}.tar.gz" gs://${bucket-name}
+gcloud storage cp "${backup_name}" gs://${GOOGLE_BUCKET_NAME}
 
 # cleanup step
-rm "blink-git-repo-${timestamp}.tar.gz"
+rm "${backup_name}"
