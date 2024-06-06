dbed8cd4f7e3f18a48c172315b70757983ac6967
Rishi Kumar <rsi.dev17@gmail.com>
2024-06-06 20:33:18 +0530

    fix: use correct syntax for gh-token

--- ci/tasks/gcp-backup.sh
+++ ci/tasks/gcp-backup.sh
@@ -11,7 +11,7 @@ EOF
 
 gcloud auth activate-service-account --key-file ${CI_ROOT}/gcloud-creds.json
 
-export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
+export GH_TOKEN="$(gh-token generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
 
 # no API keys up to 4,000 private repos.
 gh repo list ${ORG_NAME} --limit 4000 | while read -r repo _; do
